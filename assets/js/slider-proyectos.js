b37a9bbafc317349806f4e32dce28d8adace03f2
Braily03 <brailyrs03@gmail.com>
2025-06-27 23:34:50 -0400

    Seccionde tecnologias completa

--- assets/js/slider-proyectos.js
+++ assets/js/slider-proyectos.js
@@ -2,7 +2,6 @@ const $btnBack = document.querySelector(".btn-back"),
     $btnNext = document.querySelector(".btn-next"),
     $cards = document.querySelectorAll(".projects-card[data-index]");
 
-    
 export const pasarSlice = () => {
     document.addEventListener("click", e => {
         if(e.target === $btnNext || e.target.matches(".fa-arrow-right")){
@@ -24,8 +23,6 @@ export const pasarSlice = () => {
                 }
             })
         }
-
-
     })
 }
 
--- index.js
+++ index.js
@@ -2,5 +2,4 @@ import { App } from "./App.js";
 
 document.addEventListener("DOMContentLoaded", e => {
     App();
-
 });
